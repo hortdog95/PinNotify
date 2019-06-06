fae21aba77617f4c6bada0e13bccefbf267a674b
TFVC Import <ImportService@microsoft.com>
2019-06-06 03:18:15 +0000

    Imported code under path $/MainSourceRepository/PinNotify/ starting Changeset 158 TFVC History: https://hortonrepo.visualstudio.com/_versionControl/changesets?itemPath=$/MainSourceRepository/PinNotify/

---
+++ PinNotify/NotifyAction.cs
@@ -0,0 +1,114 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Text;
+using System.IO;
+
+namespace PinNotify
+{
+    interface IEmailActions
+    {
+        bool SendEmail(string[] args);
+        int RandomNumberGen();
+        string EncryptString(string val);
+        string DecryptString(string val);
+        void KeyFile();
+    }
+
+    public class NotifyAction : IEmailActions
+    {
+        private int _randomNumber = 0;
+        private string _toPerson = string.Empty;
+        private string _toAddress = string.Empty;
+        private string _fromAddress = string.Empty;
+        private string _SMTPServer = string.Empty;
+        private string _userName = string.Empty;
+        private string _password = string.Empty;
+
+        public NotifyAction(string[] args)
+        {
+            SendEmail(args);
+        }
+
+        public bool SendEmail(string[] args)
+        {
+            try
+            {
+                //decrypt the strings passed
+                _toPerson = DecryptString(args[0]);
+                _toAddress = DecryptString(args[1]);
+                _fromAddress = DecryptString(args[2]);
+                _SMTPServer = DecryptString(args[3]);
+                _userName = DecryptString(args[4]);
+                _password = DecryptString(args[5]);
+                _randomNumber = RandomNumberGen();
+
+                var message = new MimeMessage();
+                message.To.Add(new MailboxAddress(_toPerson, _toAddress));
+                message.From.Add(new MailboxAddress("Reset Pin Email", _fromAddress));
+                message.Subject = "Testing message subject";
+                message.Body = new TextPart("plain")
+                {
+                    Text = @"New PIN Number is " + _randomNumber 
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                    //Connect to SMTPServer
+                    client.Connect(_SMTPServer, 587, false);
+
+                    // Note: only needed if the SMTP server requires authentication
+                    client.Authenticate(_userName, _password);
+                    client.Send(message);
+                    client.Disconnect(true);
+
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                KeyFile();
+            }
+        }
+
+        public int RandomNumberGen()
+        {
+            //random 6 digit number generator
+            Random random = new Random();
+            _randomNumber = random.Next(99999999);
+            return _randomNumber;
+        }
+
+        public string EncryptString(string val)
+        {
+            return Convert.ToBase64String(Encoding.Unicode.GetBytes(val));
+        }
+
+
+        public string DecryptString(string val)
+        {
+            //Base64 String decryption method
+            return Encoding.Unicode.GetString(Convert.FromBase64String(val));
+        }
+
+        public void KeyFile()
+        {
+            string FileLocation = @"C:\Users\Public\Keyfile.txt";
+            string FileContent = "Pin number is " + _randomNumber.ToString();
+
+            using(var sw = new StreamWriter(FileLocation))
+            {
+                sw.WriteLine(EncryptString(FileContent));
+            }
+
+        }
+    }
+
+}
---
+++ PinNotifyTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using PinNotify;
+
+namespace PinNotifyTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] vals = new string[] {
+                "QQBsAGUAeAAgAEgAbwByAHQAbwBuAA==", //To person
+                "YQBsAGUAeAAuAGQALgBoAG8AcgB0AG8AbgBAAGgAbwB0AG0AYQBpAGwALgBjAG8AbQA=", //To Address
+                "aABvAHIAdABvAG4AbABsAGMAMQA5ADcANgBAAG8AdQB0AGwAbwBvAGsALgBjAG8AbQA=", //From Address
+                "cwBtAHQAcAAtAG0AYQBpAGwALgBvAHUAdABsAG8AbwBrAC4AYwBvAG0A", //SMTP Server
+                "aABvAHIAdABvAG4AbABsAGMAMQA5ADcANgBAAG8AdQB0AGwAbwBvAGsALgBjAG8AbQA=", //Username
+                "RABhAHIAbgBlAGwAbAA3ADYA" //password
+            };
+
+            var notify = new NotifyAction(vals);
+
+            //display the new 6 digit pin number as an encrypted string
+            Console.WriteLine("New Pin number Has been Sent to email!");
+        }
+    }
+}
