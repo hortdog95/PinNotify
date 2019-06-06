6543707294d2701d47dfe18c56f4ed7d090ebc3a
AHorton <49027817+hortdog95@users.noreply.github.com>
2019-06-05 22:29:44 -0500

    Removed Base64Strings

--- PinNotifyTest/Program.cs
+++ PinNotifyTest/Program.cs
@@ -8,12 +8,12 @@ namespace PinNotifyTest
         static void Main()
         {
             string[] vals = new string[] {
-                "QQBsAGUAeAAgAEgAbwByAHQAbwBuAA==", //To person
-                "YQBsAGUAeAAuAGQALgBoAG8AcgB0AG8AbgBAAGgAbwB0AG0AYQBpAGwALgBjAG8AbQA=", //To Address
-                "aABvAHIAdABvAG4AbABsAGMAMQA5ADcANgBAAG8AdQB0AGwAbwBvAGsALgBjAG8AbQA=", //From Address
-                "cwBtAHQAcAAtAG0AYQBpAGwALgBvAHUAdABsAG8AbwBrAC4AYwBvAG0A", //SMTP Server
-                "aABvAHIAdABvAG4AbABsAGMAMQA5ADcANgBAAG8AdQB0AGwAbwBvAGsALgBjAG8AbQA=", //Username
-                "RABhAHIAbgBlAGwAbAA3ADYA" //password
+                "", //To person
+                "", //To Address
+                "", //From Address
+                "", //SMTP Server
+                "", //Username
+                "" //password
             };
 
             var notify = new NotifyAction(vals);
